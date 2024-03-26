bea84fa7edc009d0e1e5d918f462ef708667fb82
Mariia2308 <136304734+Mariia2308@users.noreply.github.com>
2024-03-26 19:08:27 +0200

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: "/goit-react-hw-03/",
+    base: "/goit-react-hw-01/",
 plugins: [react()],
 build: {
 sourcemap: true,
